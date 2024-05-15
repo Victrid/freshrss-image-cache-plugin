d473c7168cc942c4a23fa6ab8da98eabf5e18981
Weihao Jiang <weihau.chiang@gmail.com>
2024-05-16 00:28:53 +0800

    Update with ImageProxy extension

--- i18n/en/ext.php
+++ i18n/en/ext.php
@@ -3,8 +3,12 @@
 return array(
 	'imagecache' => array(
 		'cache_url' => 'Cache URL (for user to fetch)',
-        'url_encode' => 'Encode the URL',                
+        'url_encode' => 'Encode the URL',
 		'post_url' => 'Post URL (for freshRSS to inform)',
 		'access_token' => 'Access Token (for freshRSS to inform)',
+        'proactive_cache' => 'FreshRSS Proactive Cache Settings',
+        'proactive_cache_enabled' => 'Enable proactive cache',
+        'proactive_cache_desc' => 'The proactive cache allows FreshRSS to notify cache server to download pictures ' .
+            'when new article is fetched. This will improve picture loading speed.',
 	),
-); 
+);
--- i18n/zh-CN/ext.php
+++ i18n/zh-CN/ext.php
@@ -6,5 +6,8 @@ return array(
         'url_encode' => '对URL进行转义',                
 		'post_url' => '通知URL（用于建立缓存）',
 		'access_token' => '访问token（用于建立缓存）',
+        'proactive_cache' => '主动缓存设置',
+        'proactive_cache_enabled' => '启用主动缓存',
+        'proactive_cache_desc' => '主动缓存允许FreshRSS在获取到新文章时就通知缓存服务器下载图片。这样可以加快看到图片的速度。',
 	),
 ); 
